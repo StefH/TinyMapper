9fdc146b244cfa87b3cbb1b82182663b208b8e9e
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-01-08 19:24:40 +0100

    .

--- Source/TinyMapper/Properties/AssemblyInfo.cs
+++ Source/TinyMapper/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using Nelibur.ObjectMapper.Reflection;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("97c85ab7-2580-4b03-b767-1e79088053d3")]
-[assembly: InternalsVisibleTo("UnitTests, PublicKey=4bde840421ed3b53")]
+[assembly: InternalsVisibleTo("UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d91d5600a91c47559a4101fdb29145e6f762d2c3061beecf50d1f15927546a17439f188e0239b17e1fd9eb31c9fbd900f530be120f501bcdc5eb6eae1e091e230d7dbfe19df7157cdef9c1ac16fcdf58ac0ecb824f62c702c27f92fbdc49789d24e2c63c0b9c727a6e0ed7a719a88c2286b1c2623322a4c7ab8ce51ddb4faf97")]
 [assembly: InternalsVisibleTo(DynamicAssemblyBuilder.AssemblyNameWithPublicKey)]
